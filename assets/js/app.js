1caf5ec15e84c33ea97621b38c01447366c06766
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-09-30 03:01:55 -0400

    lightbox and back button for project images

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,42 @@ left: 0,
 behavior: "smooth",
 });
 });
+
+
+// JavaScript for Lightbox
+
+// Function to open the lightbox
+function openLightbox(event) {
+    event.preventDefault();
+    const imageUrl = this.getAttribute("href");
+    const lightboxImage = document.querySelector(".lightbox-image");
+    lightboxImage.setAttribute("src", imageUrl);
+    const lightbox = document.querySelector(".lightbox");
+    lightbox.style.display = "block";
+}
+
+// Function to close the lightbox
+function closeLightbox() {
+    const lightbox = document.querySelector(".lightbox");
+    lightbox.style.display = "none";
+}
+
+// Function to go back from lightbox
+function goBack() {
+    // Implement the logic to go back from the lightbox here
+    // For example, you can close the lightbox or navigate to the previous page state.
+    closeLightbox();
+}
+
+// Add click event listeners to trigger the lightbox
+const projectImageLinks = document.querySelectorAll(".project-image-link");
+projectImageLinks.forEach((link) => {
+    link.addEventListener("click", openLightbox);
+});
+
+// Add click event listener to close the lightbox
+const closeLightboxButton = document.querySelector(".close-lightbox");
+closeLightboxButton.addEventListener("click", closeLightbox);
+
+
+
